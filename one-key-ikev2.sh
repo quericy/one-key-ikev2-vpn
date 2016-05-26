8cf1bd7eaaee334c6bdc2d566a8869dde34891f8
quericy <quericy@live.com>
2016-05-26 10:49:01 +0800

    Try to use dh14 encryption for the connection.

--- one-key-ikev2.sh
+++ one-key-ikev2.sh
@@ -299,7 +299,7 @@ conn networkmanager-strongswan
 
 conn ios_ikev2
     keyexchange=ikev2
-    ike=aes256-sha256-modp1024,3des-sha1-modp1024,aes256-sha1-modp1024!
+    ike=aes256-sha256-modp2048,3des-sha1-modp2048,aes256-sha1-modp2048!
     esp=aes256-sha256,3des-sha1,aes256-sha1!
     rekey=no
     left=%defaultroute
